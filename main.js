95b0cb0e26639a9a2b2af32781b10f7ed1d7a619
Ale-GV <67850193+Ale-GV@users.noreply.github.com>
2021-02-10 13:55:21 -0600

    just the box

--- main.js
+++ main.js
@@ -9,3 +9,17 @@ jQuery(".touchMeNot").mouseenter(function () {
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+
+/* original
+$('.touchMeNot').on('mouseenter',function(e){
+  var maxX = $(window).width() - $(this).width();
+  var maxY = $(window).height() - $(this).height();    
+  $(this).css({
+      'left':getRandomInt(0, maxX),
+      'top':getRandomInt(0, maxY)
+  });
+});
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
