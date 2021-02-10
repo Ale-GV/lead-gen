a580c3453300a9c3382b8d666673116b1504b36f
Ale-GV <67850193+Ale-GV@users.noreply.github.com>
2021-02-10 15:54:51 -0600

    another attempt

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
   $(".touchMeNot").mouseenter(function () {
     $(this).css({
       left: getRandomInt(0, maxX),
@@ -13,16 +13,3 @@ $(document).ready(function() {
     });
   });
 });
-
-/* original
-$('.touchMeNot').on('mouseenter',function(e){
-  var maxX = $(window).width() - $(this).width();
-  var maxY = $(window).height() - $(this).height();    
-  $(this).css({
-      'left':getRandomInt(0, maxX),
-      'top':getRandomInt(0, maxY)
-  });
-});
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
